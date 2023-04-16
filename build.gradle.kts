1ec50f4cee3b7d98636e060e7a8f119d794b55b9
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-04-16 12:40:53 -0500

    Delete code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,2 @@
-version = "0.2.6"
-group = "io.github.gaming32.world-host"
-
-subprojects {
-    repositories {
-        mavenCentral()
-
-        maven("https://maven.fabricmc.net/")
-
-        maven("https://maven.terraformersmc.com/releases")
-
-        maven {
-            name = "ParchmentMC"
-            url = uri("https://maven.parchmentmc.org")
-        }
-
-        maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
-
-        maven("https://jitpack.io")
-    }
-}
+version = "0.3.0"
+group = "io.github.gaming32"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,10 +9,3 @@ pluginManagement {
 }
 
 rootProject.name = "world-host"
-
-include("common")
-include("mc-1.19.2")
-include("mc-1.19.4")
-include("gui-1.19.2")
-include("gui-1.19.4")
-include("monojar")
