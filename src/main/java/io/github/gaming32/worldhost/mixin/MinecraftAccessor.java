d19493d3b8b56eb38a3b55c539c4967cf1d8c7f7
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-04-18 22:18:55 -0500

    Fix warning

--- src/main/java/io/github/gaming32/worldhost/mixin/MinecraftAccessor.java
+++ src/main/java/io/github/gaming32/worldhost/mixin/MinecraftAccessor.java
@@ -1,6 +1,5 @@
 package io.github.gaming32.worldhost.mixin;
 
-import com.mojang.authlib.AuthenticationService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
