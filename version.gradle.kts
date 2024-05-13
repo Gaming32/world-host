975ac1b9afddb3678f2c21dc17b9cda60b8dca01
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-05-13 07:24:55 -0500

    Update Gradle dependencies and pin Minotaur version

--- version.gradle.kts
+++ version.gradle.kts
@@ -13,8 +13,8 @@ plugins {
     `maven-publish`
     id("io.github.gaming32.gradle.preprocess")
     id("xyz.wagyourtail.unimined")
-    id("com.modrinth.minotaur") version "2.+"
-    id("xyz.wagyourtail.jvmdowngrader") version "0.2.0-SNAPSHOT"
+    id("com.modrinth.minotaur") version "2.8.7"
+    id("xyz.wagyourtail.jvmdowngrader") version "0.2.2"
 }
 
 fun Any.setGroovyProperty(name: String, value: Any) = withGroovyBuilder { metaClass }.setProperty(this, name, value)
