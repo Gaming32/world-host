1524c159a9ccb8ed8a8a17b33ad713ffc96ed920
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-05-22 09:06:25 -0500

    Fix proxy players being logged in in offline mode

---
+++ src/main/java/io/github/gaming32/worldhost/mixin/MixinConnection.java
@@ -0,0 +1,26 @@
+package io.github.gaming32.worldhost.mixin;
+
+import io.github.gaming32.worldhost.WorldHost;
+import io.netty.channel.Channel;
+import net.minecraft.network.Connection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.net.SocketAddress;
+
+@Mixin(Connection.class)
+public class MixinConnection {
+    @Shadow private Channel channel;
+
+    @Inject(method = "isMemoryConnection", at = @At("HEAD"), cancellable = true)
+    private void proxyConnectionDoesntCountAsMemory(CallbackInfoReturnable<Boolean> cir) {
+        final SocketAddress checkAddress = WorldHost.proxySocketAddress;
+        if (checkAddress == null) return;
+        if (checkAddress.equals(channel.localAddress()) || checkAddress.equals(channel.remoteAddress())) {
+            cir.setReturnValue(false);
+        }
+    }
+}
