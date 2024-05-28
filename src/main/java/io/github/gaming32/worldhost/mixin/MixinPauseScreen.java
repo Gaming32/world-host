a8f7baad9bc01746c52fbc842dd6f2fb08191422
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-05-28 09:42:02 -0500

    Fix build

--- src/main/java/io/github/gaming32/worldhost/mixin/MixinPauseScreen.java
+++ src/main/java/io/github/gaming32/worldhost/mixin/MixinPauseScreen.java
@@ -6,12 +6,18 @@ import io.github.gaming32.worldhost.gui.widget.OnlineStatusButton;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+//#if MC >= 1.19.4
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.injection.Redirect;
+//#else
+//$$ import org.spongepowered.asm.mixin.injection.Constant;
+//$$ import org.spongepowered.asm.mixin.injection.ModifyConstant;
+//#endif
 
 @Mixin(PauseScreen.class)
 public class MixinPauseScreen extends Screen {
