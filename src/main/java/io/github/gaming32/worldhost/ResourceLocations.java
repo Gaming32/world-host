95a9a693534f2368bcb06fee4ec6c67f15cd40b1
Josiah Glosson <soujournme@gmail.com>
2024-06-14 17:16:19 -0400

    Fix build

--- src/main/java/io/github/gaming32/worldhost/ResourceLocations.java
+++ src/main/java/io/github/gaming32/worldhost/ResourceLocations.java
@@ -3,7 +3,10 @@ package io.github.gaming32.worldhost;
 import net.minecraft.resources.ResourceLocation;
 
 public final class ResourceLocations {
-    private static final ResourceLocation WORLD_HOST_TEMPLATE = namespaced("world-host", "");
+    private static final String WORLD_HOST_NAMESPACE = "world-host";
+    //#if MC >= 1.19.4
+    private static final ResourceLocation WORLD_HOST_TEMPLATE = namespaced(WORLD_HOST_NAMESPACE, "");
+    //#endif
 
     private ResourceLocations() {
     }
@@ -17,7 +20,11 @@ public final class ResourceLocations {
     }
 
     public static ResourceLocation worldHost(String path) {
+        //#if MC >= 1.19.4
         return WORLD_HOST_TEMPLATE.withPath(path);
+        //#else
+        //$$ return new ResourceLocation(WORLD_HOST_NAMESPACE, path);
+        //#endif
     }
 
     public static ResourceLocation namespaced(String namespace, String path) {
